86e26a784ea945778759b9d45ccc8ec26ddcf160
Lautaro <lautaroceballos2013@gmail.com>
2021-05-05 18:29:47 -0300

    MongoDB Config

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 
 var mongoose = require('mongoose');
 var	app = require('./app');
-var	port = 3900;
+var	port = process.env.PORT || 3900;
+
+const user = 'admin';
+const password = 'OJIeZQpa4JVOTkbu';
+const dbname = "api-rest-blog"
+const uri = `mongodb+srv://${user}:${password}@blog-database.i95md.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 mongoose.set('useFindAndModify', false); //Para desactivar la forma de trabajar antigua y usar nuevos metodos
 mongoose.Promise = global.Promise;
 //Con promesas
-mongoose.connect('mongodb://localhost:27017/api_rest_blog', { useNewUrlParser: true })
+mongoose.connect(uri, { useNewUrlParser: true })
 	.then(() => {
 		console.log('La conexion ha sido exitosa');
 
